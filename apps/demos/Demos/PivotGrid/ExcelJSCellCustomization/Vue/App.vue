b7af5e789066df7fcc3bcf93732a0f61029bd8da
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2025-08-29 16:28:22 +0400

    add @types/file-saver-es

--- apps/demos/Demos/PivotGrid/ExcelJSCellCustomization/Vue/App.vue
+++ apps/demos/Demos/PivotGrid/ExcelJSCellCustomization/Vue/App.vue
@@ -26,7 +26,6 @@ import PivotGridDataSource from 'devextreme/ui/pivot_grid/data_source';
 import { Workbook } from 'devextreme-exceljs-fork';
 // Our demo infrastructure requires us to use 'file-saver-es'.
 // We recommend that you use the official 'file-saver' package in your applications.
-// @ts-ignore
 import { saveAs } from 'file-saver-es';
 import { exportPivotGrid, type PivotGridCell } from 'devextreme-vue/common/export/excel';
 import { sales } from './data.ts';
